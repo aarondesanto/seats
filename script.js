9396d254f97ac3afee4ecce842243855178dd2e1
adesanto <aarondesanto@gmail.com>
2015-11-10 22:44:28 -0500

    back row is a little smoother

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ function populateSeats(numberOfSeats, numberOfRows) {
 	for (var j = 0; j < ((numberOfSeats - (numberOfSeats % numberOfRows)) / numberOfRows); j++) {
 		$('.row').append("<div class='seat'></div>");
 	}
-	$('body').append("<div class='back-row'></div>");
+	$('body').append("<div class='back'></div>");
+	$('.back').append("<div class='back-row'></div>");
 	for (var k = 0; k < (numberOfSeats % numberOfRows); k++) {
 		$('.back-row').append("<div class='seat'></div>");
+	}
+	for (var l = 0; l < (numberOfRows - (numberOfSeats % numberOfRows)) / 2; l++) {
+		$('.back-row').before("<div class='not-seat'></div>");
 	}
 	return false;
 };
 
-populateSeats(144, 12);
+populateSeats(184, 12);
 
 
 });
