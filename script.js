935fc58e1864fadc6fc59a89aa4737d669f3794c
adesanto <aarondesanto@gmail.com>
2015-11-10 22:03:50 -0500

    seat population function being fleshed out

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+function populateSeats(numberOfSeats, numberOfRows) {
+	for (var i = 0; i <= numberOfRows; i++) {
+		$('body').prepend("<div class='row'></div>");
+	}
+	for (var j = 0; j <= (numberOfSeats / 4); j++) {
+		$('.row').append("<div class='seat'></div>");
+	}
+	return false;
+};
+
+populateSeats(16, 4);
 
 
 });
