cf81275a78c355e8822ea3fe816f49cdc96b8fcd
adesanto <aarondesanto@gmail.com>
2015-11-10 22:28:56 -0500

    seat population formula scaled up, can now handle uneven seat per row numbers.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
 
 function populateSeats(numberOfSeats, numberOfRows) {
-	for (var i = 0; i <= numberOfRows; i++) {
+	for (var i = 0; i < numberOfRows; i++) {
 		$('body').prepend("<div class='row'></div>");
 	}
-	for (var j = 0; j <= (numberOfSeats / 4); j++) {
+	for (var j = 0; j < ((numberOfSeats - (numberOfSeats % numberOfRows)) / numberOfRows); j++) {
 		$('.row').append("<div class='seat'></div>");
+	}
+	$('body').append("<div class='back-row'></div>");
+	for (var k = 0; k < (numberOfSeats % numberOfRows); k++) {
+		$('.back-row').append("<div class='seat'></div>");
 	}
 	return false;
 };
 
-populateSeats(16, 4);
+populateSeats(144, 12);
 
 
 });
